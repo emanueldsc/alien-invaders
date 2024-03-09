3f1acbe5218dabe34785a11a0e658865ca13d651
Emanuel Douglas Sousa Costa <emanuel.douglas.sc@gmail.com>
2024-03-09 20:39:52 -0300

    Refactory keyup and keydown events

--- game_functions.py
+++ game_functions.py
@@ -7,18 +7,24 @@ def check_events(ship):
 		if event.type == pygame.QUIT:
 			sys.exit()		
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RIGHT:
-				ship.moving_rigth = True
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = True
+			check_keydown_events(event, ship)
 		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_RIGHT:
-				ship.moving_rigth = False
-			elif event.key == pygame.K_LEFT:
-				ship.moving_left = False
+			check_keyup_events(event, ship)
 
 def update_screen(ia_settings, screen, ship):
 	# Update images in screen to new screen
 	screen.fill(ia_settings.bg_color)
 	ship.blitme()
 	pygame.display.flip()
+
+def check_keydown_events(event, ship):
+	if event.key == pygame.K_RIGHT:
+		ship.moving_rigth = True
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+
+def check_keyup_events(event, ship):
+	if event.key == pygame.K_RIGHT:
+		ship.moving_rigth = False
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
